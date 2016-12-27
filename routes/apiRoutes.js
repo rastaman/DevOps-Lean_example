5ef58d426938fd10bb82a922155efc0c8e2838fa
Johnathan Phan <jp@Rie.home>
2016-12-27 22:20:35 +0000

    example of the protected path added, new logic for seperate apps being added next

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,17 +10,6 @@ var User   = require('./../models/user'); // get our mongoose model
 mongoose.createConnection(config.database); // connect to database
 var superSecret = config.secret; // secret variable
 
-router.get('/', function(req, res) {
-  res.json({ message: 'Welcome to the coolest API on earth!' });
-});
-
-// route to return all users (GET http://localhost:8080/api/users)
-router.get('/users', function(req, res) {
-  User.find({}, function(err, users) {
-    res.json(users);
-  });
-});
-
 router.post('/authenticate', function(req, res) {
 
   // find the user
@@ -55,6 +44,48 @@ router.post('/authenticate', function(req, res) {
 
     }
 
+  });
+});
+
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+router.get('/', function(req, res) {
+  res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+router.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
   });
 });
 
