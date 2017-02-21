7074ac01c23fcaf63da965a89a86875bb617c8e4
John P <johnp@cc-g.co.uk>
2017-02-21 21:54:32 +0000

    added modified non-secure scrape request

---
+++ non-secure_microservice/README.md
@@ -0,0 +1,13 @@
+# non-secure_microservice
+
+Introduction
+
+This example will parse over a page in zoopla
+
+Prerequisites
+
+Setup user account - http://developer.zoopla.com/member/register/
+
+reference material - 
+
+https://scotch.io/tutorials/scraping-the-web-with-node-js
--- non-secure_microservice/routes/index.js
+++ non-secure_microservice/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var fs      = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/scrape', function(req, res){
+  // Let's scrape Anchorman 2
+  url = 'http://www.imdb.com/title/tt1229340/';
+
+  request(url, function(error, response, html){
+    if(!error){
+      var $ = cheerio.load(html);
+
+      var title, release, rating;
+      var json = { title : "", release : "", rating : ""};
+
+      $('.title_wrapper').filter(function(){
+        var data = $(this);
+        title = data.children().first().text().trim();
+        release = data.children().last().children().last().text().trim();
+
+        json.title = title;
+        json.release = release;
+      })
+
+      $('.ratingValue').filter(function(){
+        var data = $(this);
+        rating = data.text().trim();
+
+        json.rating = rating;
+      })
+    }
+    res.send(JSON.stringify(json))
+  })
+})
 
 module.exports = router;
