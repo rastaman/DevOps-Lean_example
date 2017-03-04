83576f6bb20f5931bb67d0b9cf28443ab9c38aec
John P <johnp@cc-g.co.uk>
2017-03-04 20:43:11 +0000

    working basic calls with no authentication, still adding logic for role check

--- 01-security_microservice/routes/apiRoutes.js
+++ 01-security_microservice/routes/apiRoutes.js
@@ -91,9 +91,7 @@ router.get('/users', function(req, res) {
 
 // route to return all users (GET http://localhost:8080/api/users)
 router.get('/roleCheck', function(req, res) {
-  User.find({}, function(err, users) {
-    res.json(users);
-  });
+  User.find();
 });
 
 
--- 03-secure_microservice/routes/index.js
+++ 03-secure_microservice/routes/index.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var elastic = require('../elasticsearch');
 var _ = require('lodash');
+var request = require('request');
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 /* GET home page. */
 router.get('/names', function(req, res) {
   
   url = 'http://localhost:3001/roleCheck';
-
-  request(url, function(error, response, html){
+  body = { "role": "03-secure_microservice" };
+  request(url, body, function(error, response, html){
     if(!error){
-      var $ = cheerio.load(html)
 
       var json = {"firstName":"John", "lastName":"Phan"};
       console.log(json);
