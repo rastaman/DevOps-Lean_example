7811c278504ad58cba5119041a78c6522dcc1624
John P <johnp@cc-g.co.uk>
2017-03-14 00:03:38 +0000

    added code for t5unamie/microservice_example/issues/3 - still not working

--- 03-secure_microservice/routes/index.js
+++ 03-secure_microservice/routes/index.js
@@ -8,17 +8,20 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/names', function(req, res) {
-  
-  url = 'http://localhost:3001/roleCheck';
-  body = { "role": "03-secure_microservice" };
+router.post('/names', function(req, res) {
+  url = 'http://localhost:3001/api/roleCheck';
+  body = { "token": req.body.token, "role": "03-secure_microservice" };
   request(url, body, function(error, response, html){
     if(!error){
-
-      var json = {"firstName":"John", "lastName":"Phan"};
-      console.log(json);
-      res.send(JSON.stringify(json))
-
+      if (response.status) {
+        var json = {"firstName":"John", "lastName":"Phan"};
+        console.log(json);
+        res.send(JSON.stringify(json))
+      } else {
+        res.json({ status: false, message: 'they lied, the security guys said they are not allowed this data' });
+      }
+    } else {
+      res.json({ status: false, message: 'broken, I am broken help me' + error });
     }
   })
 
