2d429a20acb28ed0fa5683785b20e56f4e74a230
John P <johnp@cc-g.co.uk>
2017-03-14 22:24:04 +0000

    finished secure data handeling t5unamie/microservice_example/issues/3

--- 01-security_microservice/routes/apiRoutes.js
+++ 01-security_microservice/routes/apiRoutes.js
@@ -46,36 +46,36 @@ router.post('/authenticate', function(req, res) {
   });
 });
 
-// router.use(function(req, res, next) {
+router.use(function(req, res, next) {
 
-//   // check header or url parameters or post parameters for token
-//   var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-//   // decode token
-//   if (token) {
+  // decode token
+  if (token) {
 
-//     // verifies secret and checks exp
-//     jwt.verify(token, superSecret, function(err, decoded) {      
-//       if (err) {
-//         return res.json({ success: false, message: 'Failed to authenticate token.' });    
-//       } else {
-//         // if everything is good, save to request for use in other routes
-//         req.decoded = decoded;    
-//         next();
-//       }
-//     });
+    // verifies secret and checks exp
+    jwt.verify(token, superSecret, function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
 
-//   } else {
+  } else {
 
-//     // if there is no token
-//     // return an error
-//     return res.status(403).send({ 
-//         success: false, 
-//         message: 'No token provided.' 
-//     });
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
     
-//   }
-// });
+  }
+});
 
 router.get('/', function(req, res) {
   res.json({ message: 'Welcome to the coolest API on earth!' });
