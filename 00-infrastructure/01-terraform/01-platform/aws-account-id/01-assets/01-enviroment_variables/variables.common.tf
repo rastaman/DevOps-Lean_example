9279f6bdd323e590cde458cb305cef95c6958504
John Phan <johphan@deloitte.co.uk>
2017-04-01 18:42:40 +0100

    opps forgot the variables file in the transfer

---
+++ 00-infrastructure/01-terraform/01-platform/aws-account-id/01-assets/01-enviroment_variables/variables.common.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {}
+variable "aws_profile" {}
+variable "aws_bucket" {}
+variable "providers_key_prefix" {}
+
+variable "env" {}
+variable "vpc_env" {}
+variable "project" {}
+variable "CostGroup" {}
+variable "cidr_prefix" {}
+
+variable "number_of_zones" {}
+variable "number_of_permanent_ips" {}
